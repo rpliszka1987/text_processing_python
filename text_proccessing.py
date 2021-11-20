c8c64175c40a10d4634752da046c888b2c7952e1
Robert Pliszka <robpliszka@gmail.com>
2021-11-19 20:23:59 -0500

    Added write to file and append

--- text_proccessing.py
+++ text_proccessing.py
@@ -3,5 +3,20 @@
 # This simple line of code opens the file created and prints it out.
 f = open("textfile.txt")
 print(f.read())
-
 f.close()
+
+with open("textfile.txt") as secondFile:
+    content = secondFile.read()
+
+print(content)
+
+# This is creating and writting to the file
+with open("testing.txt", "w") as myfile:
+    myfile.write("Testing\n")
+
+with open("testing.txt", "a+") as myfile:
+    myfile.write("Testing2\nTesting3\n")
+    myfile.seek(0)
+    fileContent = myfile.read()
+
+print(fileContent)
